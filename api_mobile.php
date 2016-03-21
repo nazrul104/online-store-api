e8b8b3642c92e39896a2c4be8b33e0a083413c25
Nazrul <interface12.cse@gmail.com>
2016-03-21 20:51:49 +0600

    Added files via upload

--- api_mobile.php
+++ api_mobile.php
@@ -131,6 +131,51 @@
 				    echo 'ERROR: ' . $e->getMessage();
 				}
 			}
+
+			public function getProductsById()
+			{				$id = 5;
+				try
+				{
+
+						 	  		$arr = array();
+						 	  		$proArr= array();
+						 	  			$product_query = $this->conn->prepare('SELECT * FROM products WHERE main_category_id = :main_category_id');
+										$product_query->execute(array('main_category_id' => $_REQUEST["main_category_id"]));
+									 	$productlist = $product_query->fetchAll();
+									 	if ($productlist) 
+									 	{
+									 		foreach ($productlist as $myproduct) 
+									 		{
+									 			$product =array();
+									 			$product["product_id"]=$myproduct["id"];
+									 			$product["product_title"]=$myproduct["product_title"];
+									 			$product["product_summary"]=$myproduct["product_summary"];
+									 			$product["product_descripton"]=$myproduct["product_descripton"];
+									 			$product["product_size"]=$myproduct["product_size"];
+									 			$product["product_color"]=$myproduct["product_color"];
+									 			$product["product_price"]=$myproduct["product_price"];
+
+									 			$product["product_weight"]=$myproduct["product_weight"];
+									 			$product["product_thumb_image"]=$myproduct["product_thumb_image"];
+									 			$product["product_image"]=$myproduct["product_image"];
+									 			$product["product_isActive"]=$myproduct["product_isActive"];
+									 			$product["product_stock"]=$myproduct["product_stock"];
+									 			$product["updated_date"]=$myproduct["updated_date"];
+
+									 			array_push($proArr, $product);
+									 		}
+									 		$arr["products_list"] = $proArr;
+									 	}
+
+				    echo json_encode($arr);
+				  } 
+				 catch(PDOException $e) 
+				{
+				    echo 'ERROR: ' . $e->getMessage();
+				}
+						 	 
+			}
+
 			public function Delete()
 			{	 
 			  $id=1;
